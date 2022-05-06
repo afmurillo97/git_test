50b4ae69de7221e9877e44c47eceaf8db661c97f
Andres Felipe Murillo <93634928+afmurillo97@users.noreply.github.com>
2022-05-06 12:43:43 -0500

    Optimize Re-Renders with shouldComponentUpdate

--- practice.jsx
+++ practice.jsx
@@ -188,3 +188,91 @@ class MyApp extends React.Component {
       );
     }
   }
+
+  // Add Event Listeners
+
+  class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        message: ''
+      };
+      this.handleEnter = this.handleEnter.bind(this);
+      this.handleKeyPress = this.handleKeyPress.bind(this);
+    }
+    // Change code below this line
+    componentDidMount() {
+      document.addEventListener("keydown", this.handleKeyPress)
+    }
+    componentWillUnmount() {
+      document.removeEventListener("keydown", this.handleKeyPress)
+    }
+    // Change code above this line
+    handleEnter() {
+      this.setState((state) => ({
+        message: state.message + 'You pressed the enter key! '
+      }));
+    }
+    handleKeyPress(event) {
+      if (event.keyCode === 13) {
+        this.handleEnter();
+      }
+    }
+    render() {
+      return (
+        <div>
+          <h1>{this.state.message}</h1>
+        </div>
+      );
+    }
+  };
+
+
+  // Optimize Re-Renders with shouldComponentUpdate
+
+  class OnlyEvens extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+      console.log('Should I update?');
+      // Change code below this line
+      if (nextProps.value % 2 === 0){
+        console.log(nextProps.value);
+        return true;
+      } else{
+        return false;
+      }
+      
+      // Change code above this line
+    }
+    componentDidUpdate() {
+      console.log('Component re-rendered.');
+    }
+    render() {
+      return <h1>{this.props.value}</h1>;
+    }
+  }
+  
+  class Controller extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        value: 0
+      };
+      this.addValue = this.addValue.bind(this);
+    }
+    addValue() {
+      this.setState(state => ({
+        value: state.value + 1
+      }));
+    }
+    render() {
+      return (
+        <div>
+          <button onClick={this.addValue}>Add</button>
+          <OnlyEvens value={this.state.value} />
+        </div>
+      );
+    }
+  }
