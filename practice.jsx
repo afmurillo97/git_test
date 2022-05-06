5115f3b2eb29f1482f24b91eec0d11a8b88f2302
Andres Felipe Murillo <93634928+afmurillo97@users.noreply.github.com>
2022-05-06 11:36:17 -0500

    practicing React examples

---
+++ practice.jsx
@@ -0,0 +1,190 @@
+//   create-a-controlled-input
+class ControlledInput extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        input: ''
+      };
+      // Change code below this line
+      this.handleChange = this.handleChange.bind(this)
+      // Change code above this line
+    }
+    // Change code below this line
+    handleChange(event) {
+      this.setState({
+    input: event.target.value
+  });
+  
+    }
+    // Change code above this line
+    render() {
+      return (
+        <div>
+          { /* Change code below this line */}
+          <input value = {this.state.input} onChange = {this.handleChange}></input>
+          { /* Change code above this line */}
+          <h4>Controlled Input:</h4>
+          <p>{this.state.input}</p>
+        </div>
+      );
+    }
+  };
+
+//   create-a-controlled-form
+class MyForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        input: '',
+        submit: ''
+      };
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+      this.setState({
+        input: event.target.value
+      });
+    }
+    handleSubmit(event) {
+      // Change code below this line
+          event.preventDefault()
+          this.setState({
+            submit: this.state.input
+          });
+      // Change code above this line
+    }
+    render() {
+      return (
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            {/* Change code below this line */}
+            <input value = {this.state.input} onChange = {this.handleChange}></input>
+            {/* Change code above this line */}
+            <button type='submit'>Submit!</button>
+          </form>
+          {/* Change code below this line */}
+          <h1>{this.state.submit}</h1>
+          {/* Change code above this line */}
+        </div>
+      );
+    }
+  }
+
+//   Pass State as Props to Child Components
+class MyApp extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        name: 'CamperBot'
+      }
+    }
+    render() {
+      return (
+         <div>
+           {/* Change code below this line */}
+           <Navbar name = {this.state.name}/>
+           {/* Change code above this line */}
+         </div>
+      );
+    }
+  };
+  
+  class Navbar extends React.Component {
+    constructor(props) {
+      super(props);
+  
+    }
+    render() {
+      return (
+      <div>
+        {/* Change code below this line */}
+        <h1>Hello, my name is: {this.props.name}</h1>
+        {/* Change code above this line */}
+      </div>
+      );
+    }
+  };
+
+//   Pass a Callback as Props
+class MyApp extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        inputValue: ''
+      }
+      this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(event) {
+      this.setState({
+        inputValue: event.target.value
+      });
+    }
+    render() {
+      return (
+         <div>
+          { /* Change code below this line */ }
+          <GetInput input = {this.state.inputValue} handleChange = {this.handleChange}/>
+          <RenderInput input = {this.state.inputValue} />
+          { /* Change code above this line */ }
+         </div>
+      );
+    }
+  };
+  
+  class GetInput extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h3>Get Input:</h3>
+          <input
+            value={this.props.input}
+            onChange={this.props.handleChange}/>
+        </div>
+      );
+    }
+  };
+  
+  class RenderInput extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h3>Input Render:</h3>
+          <p>{this.props.input}</p>
+        </div>
+      );
+    }
+  };
+
+
+  // Use the Lifecycle Method componentDidMount
+  class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        activeUsers: null
+      };
+    }
+    componentDidMount() {
+      setTimeout(() => {
+        this.setState({
+          activeUsers: 1273
+        });
+      }, 2500);
+    }
+    render() {
+      return (
+        <div>
+          {/* Change code below this line */}
+          <h1>Active Users: {this.state.activeUsers} </h1>
+          {/* Change code above this line */}
+        </div>
+      );
+    }
+  }
