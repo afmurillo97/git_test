8ca3f2be619fae0bc84e7a914fcfbe810b2b31e9
Andres Felipe Murillo <93634928+afmurillo97@users.noreply.github.com>
2022-05-07 10:00:41 -0500

    Use && for a More Concise Conditional

--- practice.jsx
+++ practice.jsx
@@ -276,3 +276,159 @@ class MyApp extends React.Component {
       );
     }
   }
+
+  // Add Inline Styles in React
+
+  const styles = {
+    color: "purple", 
+    fontSize: 40, 
+    border: '2px solid purple'
+  }
+  // Change code above this line
+  class Colorful extends React.Component {
+    render() {
+      // Change code below this line
+      return (
+        <div style={styles}>Style Me!</div>
+      );
+      // Change code above this line
+    }
+  };
+
+  // Use Advanced JavaScript in React Render Method
+
+  const inputStyle = {
+    width: 235,
+    margin: 5
+  };
+  
+  class MagicEightBall extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        userInput: '',
+        randomIndex: ''
+      };
+      this.ask = this.ask.bind(this);
+      this.handleChange = this.handleChange.bind(this);
+    }
+    ask() {
+      if (this.state.userInput) {
+        this.setState({
+          randomIndex: Math.floor(Math.random() * 20),
+          userInput: ''
+        });
+      }
+    }
+    handleChange(event) {
+      this.setState({
+        userInput: event.target.value
+      });
+    }
+    render() {
+      const possibleAnswers = [
+        'It is certain',
+        'It is decidedly so',
+        'Without a doubt',
+        'Yes, definitely',
+        'You may rely on it',
+        'As I see it, yes',
+        'Outlook good',
+        'Yes',
+        'Signs point to yes',
+        'Reply hazy try again',
+        'Ask again later',
+        'Better not tell you now',
+        'Cannot predict now',
+        'Concentrate and ask again',
+        "Don't count on it",
+        'My reply is no',
+        'My sources say no',
+        'Most likely',
+        'Outlook not so good',
+        'Very doubtful'
+      ];
+      const answer = possibleAnswers[this.state.randomIndex] // Change this line
+      return (
+        <div>
+          <input
+            type='text'
+            value={this.state.userInput}
+            onChange={this.handleChange}
+            style={inputStyle}
+          />
+          <br />
+          <button onClick={this.ask}>Ask the Magic Eight Ball!</button>
+          <br />
+          <h3>Answer:</h3>
+          <p>
+            {/* Change code below this line */}
+            {answer}
+            {/* Change code above this line */}
+          </p>
+        </div>
+      );
+    }
+  }
+
+
+// Render with an If-Else Condition
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true
+    }
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState((state) => ({
+      display: !state.display
+    }));
+  }
+  render() {
+    // Change code below this line
+    if (this.state.display) {
+      return (
+       <div>
+         <button onClick={this.toggleDisplay}>Toggle Display</button>
+         <h1>Displayed!</h1>
+       </div>
+    );
+    } else {
+      return (
+       <div>
+         <button onClick={this.toggleDisplay}>Toggle Display</button>
+       </div>
+    );
+    }
+    
+  }
+};
+
+// Use && for a More Concise Conditional
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true
+    }
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState(state => ({
+      display: !state.display
+    }));
+  }
+  render() {
+    // Change code below this line
+       return (
+      <div>
+         <button onClick={this.toggleDisplay}>Toggle Display</button>
+         {this.state.display && <h1>Displayed!</h1>}
+      </div>
+    );
+  }
+};
