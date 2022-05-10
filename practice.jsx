ead5e0d5f9fb9fe0736ef953d142080f807978d5
Andres Felipe Murillo <93634928+afmurillo97@users.noreply.github.com>
2022-05-10 11:15:50 -0500

    Manage State Locally First

--- practice.jsx
+++ practice.jsx
@@ -1187,14 +1187,38 @@ const store = Redux.createStore(immutableReducer);
 // Getting Started with React Redux
 
 class DisplayMessages extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       input: '',
       messages: []
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      input: e.target.value
+    })
+  }
+  submitMessage() {
+    this.setState({
+      messages: this.state.messages.concat(this.state.input),
+      input: ''
+    })
   }
   render() {
-    return <div />
+    const items = this.state.messages.map(item => <li>{item}</li>)
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        {<input 
+          onChange = {this.handleChange}
+          value = {this.state.input}
+         />}
+        {<button onClick = {this.submitMessage}>Submit</button>}
+        {<ul>{items}</ul>}
+      </div>
+    );
   }
 };
