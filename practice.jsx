4c4772caa7a804e94d790d2e730a903ae0065da1
Andres Felipe Murillo <93634928+afmurillo97@users.noreply.github.com>
2022-05-10 17:23:17 -0500

    libraries and other configurations

--- practice.jsx
+++ practice.jsx
@@ -1190,7 +1190,28 @@ const store = Redux.createStore(immutableReducer);
 // ---------------------------------- React and Redux ----------------------------------
 
 
+// Moving Forward From Here
 
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider, connect } from 'react-redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+
+import rootReducer from './redux/reducers'
+import App from './components/App'
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+);
 
 
 // Extract Local State into Redux
